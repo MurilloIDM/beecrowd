4bff581792f45b8f872a2e8d1fb01430e6f23c49
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2025-06-20 22:04:40 -0300

    new: resolve 1008

---
+++ resolutions/1008.py
@@ -0,0 +1,8 @@
+# LINK: https://judge.beecrowd.com/pt/problems/view/1008
+
+numero_funcionario = int(input())
+qtd_horas_trabalhadas = int(input())
+valor_hora = float(input())
+
+print(f'NUMBER = {numero_funcionario}')
+print(f'SALARY = U$ {(qtd_horas_trabalhadas * valor_hora):.2f}')
